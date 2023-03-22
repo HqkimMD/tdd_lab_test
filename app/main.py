534cee7351b5fe863b921218f1cf10f080ee506e
Hakim <111670192+HqkimMD@users.noreply.github.com>
2023-03-22 14:01:11 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,10 +10,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World1"}
 
-# @app.get("/callname/{name}")
-# def read_root():
-#     return {"Hello": "hakim"}
-
 @app.get("/callname/{name}")
 def read_name(name: str = None):
     return {"hello": name}
@@ -21,17 +17,5 @@ def read_name(name: str = None):
 @app.post("/callname")
 def greet_name(name: str):
     return {"hello": name}
-
-# @app.post("/callname", response_model=dict)
-# def call_name(request: NameRequest):
-#     return {"hello": request.name}
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: str = None):
-#     return {"item_id": item_id, "q": q}
-
-# @app.get("/hello/{name}")
-# def read_name(name: str = None):
-#     return {"hello": name}
 
 handler = Mangum(app)
