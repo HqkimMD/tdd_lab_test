f6f46ba85dcf0cae1ae68b8eaeb89b96e3761b88
Hakim <111670192+HqkimMD@users.noreply.github.com>
2023-03-22 13:30:01 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,17 +3,31 @@ from mangum import Mangum
 
 app = FastAPI()
 
+class NameRequest(BaseModel):
+    name: str
+    
 @app.get("/")
 def read_root():
     return {"Hello": "World1"}
 
+# @app.get("/callname/{name}")
+# def read_root():
+#     return {"Hello": "hakim"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-@app.get("/hello/{name}")
+@app.get("/callname/{name}")
 def read_name(name: str = None):
     return {"hello": name}
+
+@app.post("/callname", response_model=dict)
+def call_name(request: NameRequest):
+    return {"hello": request.name}
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str = None):
+#     return {"item_id": item_id, "q": q}
+
+# @app.get("/hello/{name}")
+# def read_name(name: str = None):
+#     return {"hello": name}
 
 handler = Mangum(app)
