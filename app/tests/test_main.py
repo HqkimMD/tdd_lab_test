f439eee2940e4be8cda3a9e9eb839ae3f48b8294
Hakim <111670192+HqkimMD@users.noreply.github.com>
2023-03-23 23:41:05 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -16,7 +16,6 @@ def test_callname():
     assert response.json() == {"hello": name}
 
 def test_callname_post():
-    name = "Hakim"
-    response = client.post("/callname", data={"name": name})
+    response = client.post("/callname")
     assert response.status_code == 200
-    assert response.json() == {"hello": name}
+    assert response.json() == {"hello": Hakim}
