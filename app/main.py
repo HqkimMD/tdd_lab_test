08489053c0d1ee67f75964382ad00db5082ade77
Hakim <111670192+HqkimMD@users.noreply.github.com>
2023-03-23 23:44:23 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def read_name(name: str = None):
     return {"hello": name}
 
 @app.post("/callname")
-    def call_name(name: "Hqkim")):
+def call_name(name: "Hqkim")):
     return {"hello": name}
 
 handler = Mangum(app)
